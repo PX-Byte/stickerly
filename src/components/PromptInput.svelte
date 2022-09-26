f97895d128316d0a86365aa0127f44089251da2b
KryptoPX <pixelbit2000@gmail.com>
2022-09-26 23:48:40 +0200

    basic login

---
+++ src/components/PromptInput.svelte
@@ -0,0 +1,34 @@
+<script type="ts">
+	let btn: HTMLButtonElement;
+	let armed = false;
+	let input = '';
+	let prompt = '';
+
+	export let text: string;
+
+	export async function promptTextbox(str: string): Promise<string> {
+		// Wait control to set def
+		while (btn == undefined) await setTimeout(() => {}, 1000);
+		armed = true;
+		return new Promise((res) => {
+			console.log('Promise started');
+			btn.addEventListener('click', () => {
+				armed = false;
+				res(input);
+				input = '';
+			});
+		});
+	}
+</script>
+
+<fieldset>
+	<legend>{text}</legend>
+	<input type="text" bind:value={input} disabled={!armed} />
+	<button bind:this={btn} disabled={!armed}>OK</button>
+</fieldset>
+
+<style>
+	fieldset {
+		width: fit-content;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script type="ts">
+	import { TGClient } from '$lib/client/TGClient';
+	import PromptInput from '../components/PromptInput.svelte';
+	import { isBrowser } from 'telegram/platform';
+
+	// const promises = {
+	// 	qCode: new Promise([]),
+	// 	qPass: new Promise([]),
+	// 	qCode: new Promise([])
+	// };
+
+	let num: string;
+	let tkn: string;
+	let pss: () => Promise<string>;
+	let cod: () => Promise<string>;
+
+	let loginClick = async () => {
+		while (pss == undefined) await setTimeout(() => console.log('Waiting pss'), 100);
+		while (cod == undefined) await setTimeout(() => console.log('Waiting cod'), 100);
+
+		const cli = new TGClient();
+		cli.queryPassword = pss;
+		cli.queryCode = cod;
+		cli.login(num);
+	};
+
+	let tokenClick = async () => {
+		while (pss == undefined) await setTimeout(() => console.log('Waiting pss'), 100);
+		while (cod == undefined) await setTimeout(() => console.log('Waiting cod'), 100);
+
+		const cli = new TGClient(tkn);
+		cli.queryPassword = pss;
+		cli.queryCode = cod;
+		cli.login(num);
+	}
+</script>
+
+<fieldset style="width: fit-content;">
+	<legend>Login</legend>
+
+	<fieldset>
+		<legend>Enter phone</legend>
+		<input type="text" bind:value={num} />
+		<button on:click={loginClick}>OK</button>
+	</fieldset>
+
+	<PromptInput bind:promptTextbox={pss} text={'Type password'} />
+	<PromptInput bind:promptTextbox={cod} text={'Type code'} />
+</fieldset>
+
+<fieldset style="width: fit-content;">
+	<legend>Login with token</legend>
+	<textarea bind:value={tkn} cols="175" rows="5" />
+	<button on:click={tokenClick}>Login</button>
+</fieldset>
